89c997b8e4a522025a237181ffab3c58eaf21e87
DEV777-web <atacanteark@gmail.com>
2023-12-11 11:10:19 -0300

    segumdo commit

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("teste")       
+function calcularRank(vitorias, derrotas) {
+  var saldoVitorias = vitorias - derrotas;
+  var nivel;
+
+  if (vitorias <= 10) {
+   nivel = "Ferro1";
+  } else if (vitorias >= 11 && vitorias <= 20) {
+  nivel = "Bronze2";
+  } else if (vitorias >= 21 && vitorias <= 50) {
+     nivel = "Prata3";
+  } else if (vitorias >= 51 && vitorias <= 80) {
+    nivel = "Ouro4";
+  } else if (vitorias >= 81 && vitorias <= 90) {
+    nivel = "Diamante5";
+  } else if (vitorias >= 91 && vitorias <= 100) {
+    nivel = "Lendário6";
+  } else {
+    nivel = "Imortal10";
+  }
+
+  var mensagem = "O Herói tem um saldo de " + saldoVitorias + " e está no nível de " + nivel;
+  return mensagem;
+}
+
+
+var quantidadeVitorias = parseInt(prompt("Digite a quantidade de vitórias:"));
+var quantidadeDerrotas = parseInt(prompt("Digite a quantidade de derrotas:"));
+
+var resultado = calcularRank(quantidadeVitorias, quantidadeDerrotas);
+console.log(resultado);
